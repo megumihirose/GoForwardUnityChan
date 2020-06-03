b093efc8a25b8acce1be51eb98768bec7d32dcdc
megumihiroserose <hirosemworks@gmail.com>
2020-06-03 20:18:06 +0900

    よろしくおねがいします

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    Animator animator;
+    // 地面の位置
+    private float groundLevel = -3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // アニメータのコンポーネントを取得する
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 走るアニメーションを再生するために、Animatorのパラメータを調節する
+        this.animator.SetFloat("Horizontal", 1);
+
+        // 着地しているかどうかを調べる
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+    }
+}
